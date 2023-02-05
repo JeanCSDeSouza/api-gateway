a3f39b6e84de85b976fd167c9efdece3f39da760
Jean Carlos <jcsouza.first@gmail.com>
2023-02-05 13:45:13 -0300

    Atualização do filtro de segurança

--- src/main/java/br/com/livrariareal/apigateway/ApiGatewayApplication.java
+++ src/main/java/br/com/livrariareal/apigateway/ApiGatewayApplication.java
@@ -2,10 +2,11 @@ package br.com.livrariareal.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @Author Jean Carlos
- * Habilita o spring cloud gateway
+ * Classe principal da api gateway
  */
 @SpringBootApplication
 public class ApiGatewayApplication {
